31369d7ea4664f2ec1f044314b7a8afaadf79fc4
G K <gk@setup.com>
2026-03-13 16:09:32 -0700

    css

--- app/components/ContactModal.tsx
+++ app/components/ContactModal.tsx
@@ -20,7 +20,7 @@ export default function ContactModal() {
         pointerEvents: isOpen ? "all" : "none",
         transition: "opacity 0.3s ease",
       }}
-      className="items-start lg:items-center"
+      className="items-start! lg:items-center!"
     >
       <div
         onClick={(e) => e.stopPropagation()}
